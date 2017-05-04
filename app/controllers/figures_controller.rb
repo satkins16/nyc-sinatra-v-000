bab7da444f6a6a912428ccf84b0526ede7a74719
Scott Atkins <satkins16@gmail.com>
2017-05-04 03:39:14 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,8 +4,15 @@ class FiguresController < ApplicationController
   end
 
   post 'figures' do
-    @figure = Figure.new(name: params[figure[name]])
-    
+    @figure = Figure.create(params[:figure])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    @figure.save
+
   end
 
 end
